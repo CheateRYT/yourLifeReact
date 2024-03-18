69da38b9342c2e6619e05a68e497ec7bedfe379c
CheateRYT <cheaterytyt@gmail.com>
2024-03-18 23:36:37 +0300

    historyAutoScroll added

--- src/Components/GameComponents/GameMain.jsx
+++ src/Components/GameComponents/GameMain.jsx
@@ -1,21 +1,29 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import '../scss/GameMain.scss';
 import { UserContext } from '../../Contexts/UserContext';
+
 export const GameMain = () => {
   const { user, updateAge, setUser } = useContext(UserContext);
+  const historyRef = useRef(null);
+
   const handleUpdateAge = () => {
     user.age++;
     user.updateLocalStorage();
   };
+
+  useEffect(() => {
+    if (historyRef.current) {
+      historyRef.current.scrollTop = historyRef.current.scrollHeight;
+    }
+  }, [user]);
+
   return (
     <div className="game-main">
-      <div className="history">
+      <div ref={historyRef} className="history">
         <h1 className="history__title">История</h1>
         <span className="history__text">
-          randomtext randomtext randomtext randomtext randomtext random rand
-          dsssdddddddddddddddddddddddsssdddddddddddddddddddddddsssdddddddddddddddddddddddsssdddddddddddddddddddddddsssdddddddddddddddddddddddsssdddddddddddddddddddddddsssdddddddddddddddddddddd
+          randomtext randomtext randomtext randomtext randomtext randomtext
         </span>
-        <span className="history__text">new</span>
       </div>
       <div onClick={handleUpdateAge} className="age-button">
         <span className="age-button__plus">+</span>
