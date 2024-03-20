92e375917c6331b10eca69d0b7a34a8225c6b834
CheateRYT <cheaterytyt@gmail.com>
2024-03-20 20:33:19 +0300

    exit to main menu and start new game after death

--- src/Components/DeathPopup.jsx
+++ src/Components/DeathPopup.jsx
@@ -5,9 +5,14 @@ import { UserContext } from '../Contexts/UserContext';
 import emptyProfile from '../img/emptyProfile.png';
 import femaleProfile from '../img/femaleProfile.png';
 import maleProfile from '../img/maleProfile.png';
+import clickButtonAudio from '../Utils/clickButtonAudio.mp3';
+import { useNavigate } from 'react-router-dom';
+import { CreateGamePopup } from './popups/CreateGamePopup';
 export const DeathPopup = ({ hideDeathPopup }) => {
   const { user } = useContext(UserContext);
   const [profileImage, setProfileImage] = useState(emptyProfile);
+  const [showCreateGame, setShowCreateGame] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
     if (user && user.gender) {
       setProfileImage(user.gender === 'female' ? femaleProfile : maleProfile);
@@ -15,6 +20,16 @@ export const DeathPopup = ({ hideDeathPopup }) => {
       setProfileImage(emptyProfile);
     }
   }, [user]);
+  const handleMainMenuExit = () => {
+    const audio = new Audio(clickButtonAudio);
+    audio.play();
+    navigate('/');
+  };
+  const handleNewGame = () => {
+    const audio = new Audio(clickButtonAudio);
+    audio.play();
+    setShowCreateGame(true);
+  };
   return (
     <div className="death-popup popup-narrow">
       <span
@@ -39,11 +54,16 @@ export const DeathPopup = ({ hideDeathPopup }) => {
           <span className="death-diseases">Болезни: {user.diseases}</span>
 
           <div className="death-buttons">
-            <button className="death-button">Выйти в главное меню</button>
-            <button className="death-button">Начать заново</button>
+            <button onClick={handleMainMenuExit} className="death-button">
+              Выйти в главное меню
+            </button>
+            <button onClick={handleNewGame} className="death-button">
+              Начать заново
+            </button>
           </div>
         </div>
       </div>
+      {showCreateGame && <CreateGamePopup />}
     </div>
   );
 };
